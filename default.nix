99b612b173b6ca7256ae1bd10b98b34f7a210577
Sean Hickman <sean@grumply.com>
2020-02-13 23:28:50 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure-core, pure-default, stdenv }:
 mkDerivation {
   pname = "excelsior";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure-core pure-default ];
   license = stdenv.lib.licenses.bsd3;
